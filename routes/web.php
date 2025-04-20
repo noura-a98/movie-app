c462803b9bd9e9d72421659a292c15ce432c8ad0
noura-a98 <nouraaaa108@gmail.com>
2025-04-20 22:27:27 +0400

    feat(routes): switch to controller-based routing in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::view('/','layouts.index');
-Route::view('/movie','layouts.show');
-
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
