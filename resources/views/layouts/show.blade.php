4fd6d3502653f5b2af903ac93010b946a1a147c0
noura-a98 <nouraaaa108@gmail.com>
2025-04-22 23:19:53 +0400

    feat(layouts) : fix the feature

--- resources/views/layouts/show.blade.php
+++ resources/views/layouts/show.blade.php
@@ -7,24 +7,30 @@
 
     <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
         <div class="flex flex-none">
-            <img src="{{ asset(path: 'img/parasite.jpg') }}" alt="parasite" class="w-64 md:w-96" style="width: 24rem">
+            <img src="https://image.tmdb.org/t/p/w500{{ $movie['poster_path'] }}" alt="parasite" class="w-64 lg:w-96">
         </div>
         <div class="md:ml-24">
-            <h2 class="text-4xl font-semibold">Parasite (2019)</h2>      
+            <h2 class="text-4xl font-semibold">{{$movie['title']}}</h2>      
             <div class="flex flex-wrap items-center text-gray-400 text-sm">
           <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24">
             <g data-name="Layer 2">
               <path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star" />
             </g>
           </svg>
-          <span class="md:ml-1 text">85%</span>
+          <span class="ml-1 text">{{floor($movie['vote_average'] * 10) . '%' }}</span>
           <span class="md:mx-2">|</span>
-          <span>Feb 20, 2020</span>
+          <span>{{\Carbon\Carbon::parse($movie['release_date']) -> format('M d , Y')}}</span>
           <span class="sm:mx-2">|</span>
-          <span>Action, Thriller, Drama</span>
+          <span>  
+            <div class="text-gray-400 text-sm">
+              @foreach ($movie['genres'] as $genre )
+                {{ $genre['name'] }} @if(!$loop -> last), @endif
+                @endforeach
+          </div>  
+      </span>
         </div> 
         <p class="text-gray-300 mt-8">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi qui ex tempore totam! Autem consequatur repudiandae aut aliquid amet blanditiis voluptatum maxime corrupti quidem nam? Repellat ipsa doloribus error.
+          {{ $movie['overview'] }}
         </p>
         
         <div class="mt-12">
